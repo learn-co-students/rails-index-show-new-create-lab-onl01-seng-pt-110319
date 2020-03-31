43f8afbc617c9356e621486b854b8c0e6e01b801
Warren Harper II <warrenharper2@att.net>
2020-03-31 19:22:43 +0000

    Done.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
-describe Coupon do
-  before(:each) do
-    @coupon = Coupon.create!(coupon_code: "FREESTUFF", store: "Chipotle")
-  end
+#RSpec.describe Coupon, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
 
-  it 'can be created' do
-    expect(@coupon).to be_valid
-  end
-end
+#end
